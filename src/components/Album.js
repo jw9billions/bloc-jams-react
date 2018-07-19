0f5115a160606210eae0585fc121ed011ab29ecc
Jane Wang <jw9billions@gmail.com>
2018-07-18 20:37:27 -0700

    cp5assignment display the song list in an album

--- src/components/Album.js
+++ src/components/Album.js
@@ -28,10 +28,19 @@ class Album extends Component {
         <table id="song-list">
           <colgroup>
             <col id="song-number-column" />
-            <col id="song-number-column" />
+            <col id="song-title-column" />
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
+            {
+              this.state.album.songs.map( (song, index) =>
+                <tr key={index}>
+                  <td className="song-number">{index+1}</td>
+                  <td className="song-title">{song.title}</td>
+                  <td className="song-duration">{song.duration}</td>
+                </tr>
+              )
+            }
           </tbody>
         </table>
       </section>

62e01250d4b3fe29daacebfa633a48737e36ccf0
Jane Wang <jw9billions@gmail.com>
2018-08-04 17:56:54 -0700

    formatTime on PlayBar

--- src/components/Album.js
+++ src/components/Album.js
@@ -88,6 +88,15 @@ class Album extends Component {
     this.setState({ currentTime: newTime });
   }
 
+  formatTime(seconds) {
+    if (isNaN(seconds)) {
+      return "-:--";
+    }
+    let M = Math.floor( seconds/60 );
+    let SS = (seconds % 60).toFixed(2);
+    return M + ":" + SS;
+  }
+
   render() {
     return (
       <section className="album">
@@ -117,7 +126,7 @@ class Album extends Component {
                     </botton>
                   </td>
                   <td className="song-title">{song.title}</td>
-                  <td className="song-duration">{song.duration}</td>
+                  <td className="song-duration">{this.formatTime(song.duration)}</td>
                 </tr>
               )
             }
@@ -132,6 +141,7 @@ class Album extends Component {
           handlePrevClick={() => this.handlePrevClick()}
           handleNextClick={() => this.handleNextClick()}
           handleTimeChange={(e) => this.handleTimeChange(e)}
+          formatTime={(e) => this.formatTime(e) }
         />
       </section>
     );
